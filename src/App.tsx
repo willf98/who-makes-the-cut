cd228c0b9b4cb0e5280fe498c2d7fca7cfbee90f
willf98 <willf98@byu.edu>
2026-05-04 17:48:03 -0600

    Connecting to supabase

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,9 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-
 function App() {
   const [count, setCount] = useState(0)
-  
+
   return (
     <>
       <section id="center">
